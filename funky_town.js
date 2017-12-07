1be4baa171413c95528bb93cedf0d87deafc0919
Bayan Berri <bberri@stuy.edu>
2017-12-06 19:10:49 -0500

    gcd

--- funky_town.js
+++ funky_town.js
@@ -7,3 +7,24 @@ var fibonacci= function(n){
   };
   return fibonacci(n-1)+fibonacci(n-2);
 };
+
+var gcd= function(a,b){
+  a= Math.abs(a);
+  b=Math.abs(b);
+  if(b>a){
+    var temp=a;
+    a=b;
+    b=temp;
+
+  };
+  while(true){
+    if(b==0){
+      return a;
+    };
+    a=a%b;
+    if(a==0){
+      return b;
+    }
+    b=b%a;
+  };
+}
