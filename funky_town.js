2993eea35938db776c819f74138c823968d6d57c
Ibnul Jahan <ijahan1@stuy.edu>
2017-12-08 02:02:07 -0500

    Phase two

--- funky_town.js
+++ funky_town.js
@@ -85,6 +85,58 @@ console.log("This was the list: " + list);
 console.log("");
 
 
+//------------------------------PHASE 2--------------------------//
+
+
+//THESE ARE THE GENERIC BUTTON CALLBACK FUNCTIONS
+var buttonCallback = function(e){
+    console.log("\n\nhere comes e...");
+    console.log(e);
+    console.log("\n\nhere comes 'this'...");
+    console.log(this);
+};
+var b = document.getElementById('b');
+b.addEventListener('click', buttonCallback);
+
+
+//Now, for our phase 1 functions
+
+//Callback button for fibonacci
+//Returns a random fibonacci number between 4 and 43
+var fibCallback = function(e){
+    //the only reason for the + 4 is because every number 4 and higher is followed by a 'th term'
+    var nthTerm = Math.floor(Math.random()*40) + 4;
+    console.log("\nThis is the " + nthTerm + "th term of the Fibonacci sequence");
+    console.log("fibonacci(" + nthTerm + ") -> " + fibonacci(nthTerm));
+};
+//Event listener for fibonacci
+var fibButton = document.getElementById('fibonacci');
+fibButton.addEventListener('click', fibCallback);
+
+//Callback button for gcd
+//Returns the gcd between 2 random numbers between 1 and 500
+var gcdCallback = function(e){
+    //these are the two numbers
+    var a = Math.floor(Math.random()*500);
+    var b = Math.floor(Math.random()*500);
+    console.log("\nThis is the GCD of " + a + " and " + b);
+    console.log("gcd(" + a + "," + b + ") -> " + gcd(a,b));
+};
+//Event listener for GCD
+var gcdButton = document.getElementById('gcd');
+gcdButton.addEventListener('click', gcdCallback);
+
+//Callback button for randomStudent
+//Returns 1 (one) random name from the list
+var randStudentCallback = function(e){
+    console.log("\nHere is a random name from this list: " + list);
+    console.log("randomStudent(list) -> " + randomStudent(list));
+};
+//Event listener for randStudent
+var randStudentButton = document.getElementById('randomStudent');
+randStudentButton.addEventListener('click', randStudentCallback);
+
+
 
 //SIGNING OFF
 console.log("This has been a production by Bayan & Ibnul");
